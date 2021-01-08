9be4072d2dae27e1b33ec8646802a340a90355be
regan mabika <r.mabika@outlook.com>
2021-01-08 13:39:37 +0200

    App Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
+
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
+import { DeletePostComponent } from './components/delete-post/delete-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCRUDExample';
+  postList: any[] = [];
+  bsModalRef: BsModalRef;
+
+  constructor(private blogService: BlogService, private bsModalService: BsModalService) {
+    this.getPosts();
+  }
+
+  getPosts() {
+    this.blogService.getPostList().subscribe(data => {
+      Object.assign(this.postList, data);
+    }, error => {
+      console.log("Error while getting posts ", error);
+    });
+  }
+
+  addNewPost() {
+    this.bsModalRef = this.bsModalService.show(AddNewPostComponent);
+    this.bsModalRef.content.event.subscribe(result => {
+      if (result == 'OK') {
+        this.getPosts();
+      }
+    });
+  }
+
+  deletePost(postId: number, title: string) {
+    this.bsModalRef = this.bsModalService.show(DeletePostComponent);
+    this.bsModalRef.content.postId = postId;
+    this.bsModalRef.content.title = title;
+    this.bsModalRef.content.event.subscribe(result => {
+      console.log("deleted", result);
+      if (result == 'OK') {
+        setTimeout(() => {
+          this.postList=[];
+          this.getPosts();
+        }, 5000);
+      }
+    });
+  }
+
+  editPost(postId: number) {
+    this.blogService.changePostId(postId);
+
+    this.bsModalRef = this.bsModalService.show(EditPostComponent);
+    this.bsModalRef.content.event.subscribe(result => {
+      if (result == 'OK') {
+        setTimeout(() => {
+          this.getPosts();
+        }, 5000);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogService } from 'src/app/services/blog.service';
+import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
+import { DeletePostComponent } from './components/delete-post/delete-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNewPostComponent,
+    EditPostComponent,
+    DeletePostComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BlogService, BsModalService],
+  bootstrap: [AppComponent],
+  entryComponents:[AddNewPostComponent, DeletePostComponent, EditPostComponent]
 })
 export class AppModule { }
